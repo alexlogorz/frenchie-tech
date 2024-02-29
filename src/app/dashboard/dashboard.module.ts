d908d64d6a41d80b26eef3884b1031241ec002a5
alex logorz <alex.logorz@gmail.com>
2024-02-29 14:13:25 -0500

    renamed menu to sidenav and added routing path for the dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserprofileComponent } from './userprofile/userprofile.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    UserprofileComponent
+    UserprofileComponent,
+    SidenavComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    UserprofileComponent
+    UserprofileComponent,
+    SidenavComponent,
+    DashboardComponent
   ]
 })
 export class DashboardModule { }
